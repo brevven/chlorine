95c9e70404ff524fd8df097130f9a2fed48bce05
Brevven <brevingame@gmail.com>
2022-07-26 17:55:31 -0700

    me

---
+++ me.lua
@@ -0,0 +1,27 @@
+local me = {}
+
+me.name = "bzchlorine"
+me.list = {}
+
+function me.get_setting(name)
+  if settings.startup[name] == nil then
+    return nil
+  end
+  return settings.startup[name].value
+end
+
+me.bypass = {}
+if me.get_setting(me.name.."-recipe-bypass") then 
+  for recipe in string.gmatch(me.get_setting(me.name.."-recipe-bypass"), '[^",%s]+') do
+    me.bypass[recipe] = true
+  end
+end
+
+function me.add_modified(name) 
+  if me.get_setting(me.name.."-list") then 
+    table.insert(me.list, name)
+  end
+end
+
+
+return me
