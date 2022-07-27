a7d0e301608a0c479ec77683c6862597f40e403b
Brevven <brevingame@gmail.com>
2022-07-27 02:06:03 -0700

    settings

---
+++ settings.lua
@@ -0,0 +1,28 @@
+data:extend({
+  {
+		type = "string-setting",
+		name = "bzchlorine-recipe-bypass",
+		setting_type = "startup",
+		default_value = "",
+    allow_blank = true,
+	},
+  {
+		type = "bool-setting",
+		name = "bzchlorine-list",
+		setting_type = "startup",
+    default_value = false,
+	},
+  {
+    type = "bool-setting",
+    name = "bzchlorine-salt",
+		setting_type = "startup",
+    default_value = false,
+  },
+  {
+		type = "string-setting",
+		name = "bzchlorine-more-intermediates",
+		setting_type = "startup",
+		default_value = "yes",
+    allowed_values = {"yes", "no"},
+	},
+})
