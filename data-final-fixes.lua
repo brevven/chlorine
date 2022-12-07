b177d90daf3c3a23307871131903ce7ed35f4c15
Brevven <brevingame@gmail.com>
2022-12-07 01:14:04 -0800

    core

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -8,6 +8,11 @@ local util = require("data-util");
 local pg_amt = util.get_ingredient_amount("plastic-bar", "petroleum-gas")
 util.replace_ingredient("plastic-bar", "petroleum-gas", "vinyl-chloride", pg_amt*2/3)
 
+if mods["space-exploration"] then 
+  -- core mining balancing
+  util.add_to_product("se-core-fragment-omni", "salt", -5)
+end
+
 
 -- Must be last
 util.create_list()
