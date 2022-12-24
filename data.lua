4cd19cbf643a0ce4e77d68a734e220fd87aa71c7
Brevven <brevingame@gmail.com>
2022-12-24 01:08:35 -0800

    delivery cannon

--- data.lua
+++ data.lua
@@ -2,11 +2,12 @@ require("prototypes/salt")
 require("prototypes/chlorine")
 require("prototypes/epoxy")
 require("prototypes/pcb")
--- require("tin-recipe")
--- require("tin-enriched")   -- Enriched Al for Krastorio 2
--- require("tin-recipe-se")         -- Space Exploration
 
 local util = require("data-util");
+
+if util.se6() then
+  se_delivery_cannon_recipes["salt"] = {name="salt"}
+end
 
 -- Must be last
 util.create_list()
