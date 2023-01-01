634e7f6d8d795b230b4edb60e0cce1ab6d2e4367
Brevven <brevingame@gmail.com>
2023-01-01 01:07:00 -0800

    modmash splinter subspace

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -13,6 +13,61 @@ if mods["space-exploration"] then
   util.add_to_product("se-core-fragment-omni", "salt", -5)
 end
 
+if mods.modmashsplintersubspacelogistics then
+  for i, item in pairs({"salt"}) do
+    if data.raw.item["super-container-for-"..item] then
+      for i, icon in pairs(data.raw.item["super-container-for-"..item].icons) do
+        if string.find(icon.icon, item) then
+          icon.size = 128
+          icon.scale = icon.scale / 2
+          icon.icon_scale = icon.icon_scale / 2
+        end
+      end
+    end
+    if data.raw.recipe["super-container-for-"..item] then
+      for i, icon in pairs(data.raw.recipe["super-container-for-"..item].icons) do
+        if string.find(icon.icon, item) then
+          icon.size = 128
+          icon.scale = icon.scale / 4
+          icon.icon_scale = icon.icon_scale / 4
+        end
+      end
+    end
+    if data.raw.recipe["empty-super-container-of-"..item] then
+      for i, icon in pairs(data.raw.recipe["empty-super-container-of-"..item].icons) do
+        if string.find(icon.icon, item) then
+          icon.size = 128
+          icon.scale = icon.scale / 4
+          icon.icon_scale = icon.icon_scale / 4
+        end
+      end
+    end
+    if data.raw.recipe["modmash-supermaterial-to-"..item] then
+      for i, icon in pairs(data.raw.recipe["modmash-supermaterial-to-"..item].icons) do
+        if string.find(icon.icon, item) then
+          icon.size = 128
+          icon.scale = icon.scale / 4
+          icon.icon_scale = icon.icon_scale / 4
+        end
+      end
+    end
+    for i, technology in pairs(data.raw.technology) do
+      if string.find(technology.name, "containment") or string.find(technology.name, "supermaterial") then
+        if technology.icons then
+          for j, icon in pairs(technology.icons) do
+            if string.find(icon.icon, item) then
+              icon.size = 128
+              icon.scale = icon.scale / 8
+              icon.icon_scale = icon.icon_scale / 8
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
 
 -- Must be last
 util.create_list()
