69cf0ec32416fad7e38872e88f1941a2f5663046
Brevven <brevingame@gmail.com>
2023-01-05 17:46:39 -0800

    gdiw

--- data-updates.lua
+++ data-updates.lua
@@ -10,6 +10,8 @@ require("prototypes/assembler-updates")
 
 local util = require("data-util")
 
+local pg_amt = util.get_ingredient_amount("plastic-bar", "petroleum-gas")
+util.replace_ingredient("plastic-bar", "petroleum-gas", "vinyl-chloride", pg_amt*2/3)
 
 
 require("modules")
